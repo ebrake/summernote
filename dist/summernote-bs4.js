f8f2fc7371594c380bee7d98ac7a01406d3d0f44
E Brake <ebrake@recruitology.com>
2018-02-28 16:49:36 -0600

    dist build

--- dist/summernote-bs4.js
+++ dist/summernote-bs4.js
@@ -5,7 +5,7 @@
  * Copyright 2013- Alan Hong. and other contributors
  * summernote may be freely distributed under the MIT license.
  *
- * Date: 2018-02-28T21:48Z
+ * Date: 2018-02-28T22:48Z
  */
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? factory(require('jquery')) :
